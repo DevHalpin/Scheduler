4264ca05a5c02e3dc48da7a012cd802bfa8f17fc
David Halpin <devhalpin86@gmail.com>
2020-09-30 19:14:33 -0700

    Clean up helper function

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,16 +1,12 @@
 export function getAppointmentsForDay(state,day) {
   const filteredDays = state.days.filter(days => days.name === day);
-  if (!filteredDays[0]) {
+  const dateFound = filteredDays[0];
+  if (!dateFound) {
     return [];
   } else {
-    let filteredAppointments = [];
-    state.days.forEach((perDay) => {
-      if (perDay.name === day) {
-        for (const perApt of perDay.appointments) {
-          filteredAppointments.push(state.appointments[perApt]);
-        }
-      }
-    });
+    const filteredAppointments = dateFound.appointments.map(perApt => {
+      return state.appointments[perApt];
+    })
     return filteredAppointments;
   }
 }
