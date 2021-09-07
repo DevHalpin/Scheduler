a2717770cee9174213f628ec5949e0b285bed854
David Halpin <echidnablade@hotmail.com>
2021-09-06 20:42:04 -0700

    clean up duplicate code in transition

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -4,20 +4,11 @@ export default function useVisualMode(initial) {
   const [history, setHistory] = useState([initial]);
 
   const transition = function(mode, replace = false) {
-    if (replace) {
-      setHistory(prev => [...prev.slice(0, -1), mode]);
-    } else {
-      setHistory(prev => [...prev, mode]);
-    }
+   setHistory(prev => replace? [...prev.slice(0,-1), mode] : [...prev, mode]);
   }
 
   const back = function () {
-    setHistory(prev => {
-      if (prev.length > 1) {
-        return prev.slice(0, -1)
-      }
-      return prev
-    })
+    setHistory(prev => prev.length > 1 ? prev.slice(0, -1) : prev);
   }
 
   return { mode: history[history.length -1], transition, back };
